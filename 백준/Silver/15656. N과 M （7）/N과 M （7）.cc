615dc350a8f57b3a606fe9f12103980db2238471
강태현 <126179088+KNU-K@users.noreply.github.com>
2023-10-07 14:37:30 +0900

    [Silver III] Title: N과 M (7), Time: 704 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int visited[9] = { 0, };
+int arr[9];
+int result[9];
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << result[i] <<" ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (!visited[i]) {
+			result[cnt] = arr[i];
+			dfs(cnt + 1);
+		}
+	}
+
+}
+
+
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr, arr + n);
+	dfs(0);
+}
