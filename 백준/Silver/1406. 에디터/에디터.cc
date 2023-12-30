33bfcb1984808ce1350b78fb1c8cd2664b7d55bd
강태현 <126179088+KNU-K@users.noreply.github.com>
2023-12-30 20:22:23 +0900

    [Silver II] Title: 에디터, Time: 36 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,65 +1,57 @@
 #include <iostream>
 #include <stack>
-#include <string>
+#include <queue>
 using namespace std;
-
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios::sync_with_stdio(false);
-
-    string str;
-    stack<char> left, right;
-    char cmd, a;
-
-    cin >> str;
-
-    for (char ch : str) {
-        left.push(ch);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	string str;
+	stack<char> s;
+	stack<char> r;
+	char cmd;
+	cin >> str;
+	for (int i = 0; i < str.length();i++) {
+		s.push(str[i]);
+	}
+	int n;
+	
+	char a;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> cmd;
+		switch (cmd) {
+		case 'P':
+			cin >> a;
+			s.push(a);
+			break;
+		case 'L':
+			if (!s.empty()) {
+				r.push(s.top());
+				s.pop();
+			}
+			break;
+		case 'D':
+			if(!r.empty()){
+				s.push(r.top());
+				r.pop();
+			}
+			break;
+		case 'B':
+			if (!s.empty()) {
+				s.pop();
+			}
+		}
+	}
+	while (!s.empty()) {
+        r.push(s.top());
+        s.pop();
     }
 
-    int n;
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cin >> cmd;
-
-        switch (cmd) {
-        case 'P':
-            cin >> a;
-            left.push(a);
-            break;
-        case 'L':
-            if (!left.empty()) {
-                right.push(left.top());
-                left.pop();
-            }
-            break;
-        case 'D':
-            if (!right.empty()) {
-                left.push(right.top());
-                right.pop();
-            }
-            break;
-        case 'B':
-            if (!left.empty()) {
-                left.pop();
-            }
-            break;
-        }
-    }
-
-    while (!left.empty()) {
-        right.push(left.top());
-        left.pop();
-    }
-
-    while (!right.empty()) {
-        cout << right.top();
-        right.pop();
+    while (!r.empty()) {
+        cout << r.top();
+        r.pop();
     }
 
     cout << "\n";
-
-    return 0;
 }
