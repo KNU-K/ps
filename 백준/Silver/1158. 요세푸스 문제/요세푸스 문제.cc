434af4c6570f067babc41cfd43f9f152f25d1ee2
강태현 <126179088+KNU-K@users.noreply.github.com>
2023-12-31 22:33:13 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 112 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int n, k;
+	queue<int> q;
+	cin >> n>>k;
+	for (int i = 1; i <= n;i++) {
+		q.push(i);
+	}
+	int cnt = 1;
+	cout << "<";
+	while (!q.empty()) {
+		if (cnt % k == 0) {
+			if (q.size() == 1) {
+				cout << q.front() ;
+			}
+			else {
+				cout << q.front() << ", ";
+			}
+			q.pop();
+		}
+		else {
+			q.push(q.front());
+			q.pop();
+		}
+		cnt++;
+	}
+
+	cout << ">\n";
+}
