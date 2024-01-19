d8f57cafae595aaf64f901f5c4d14bec2ba5a48d
강태현 <126179088+KNU-K@users.noreply.github.com>
2024-01-19 22:46:27 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 16 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[10001];
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, M; cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        int v, c, k; cin >> v >> c >> k;
+        int t = 1;
+        int temp;
+        while (k > 0) {
+            temp = min(k, t);
+            for (int j = M; j >= v * temp; j--) {
+                dp[j] = max(dp[j - v * temp] + c * temp, dp[j]);
+            }
+            t *= 2;
+            k -= temp;
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i <= M; i++) ans = max(dp[i], ans);
+    cout << ans;
+}
