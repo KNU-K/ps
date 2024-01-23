fe874531685dd3b08f09103352fdf17f73f210ee
강태현 <126179088+KNU-K@users.noreply.github.com>
2024-01-24 00:09:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[1001] = { 0, };
+int lis[1001] = { 0, };
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	lis[0] = arr[0];
+	int k = 0;
+	for (int i = 1; i < n; i++) {
+		if (lis[k]<arr[i]) {
+			lis[++k] = arr[i];
+		}
+		else {
+			int* idx = lower_bound(lis, lis + k+1, arr[i]);
+			*idx = arr[i];
+		}
+	}
+	cout << k + 1;
+
+}
