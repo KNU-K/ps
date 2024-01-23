ddc0f58a5295a9d1176929a2c3bb3285c3552cda
강태현 <126179088+KNU-K@users.noreply.github.com>
2024-01-24 04:50:36 +0900

    [Silver II] Title: 민균이의 계략, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+int arr[1001];
+int lis[1001];
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+	lis[1] = arr[1];
+	int k = 1;
+	for (int i = 2; i <= n; i++) {
+		if (lis[k] < arr[i]) {
+			lis[++k] = arr[i];
+		}
+		else {
+			auto it = lower_bound(lis, lis + k, arr[i]);
+			(*it) = arr[i];
+		}
+	}
+	cout << k << "\n";
+
+}
+//4 5 3 2 1
+//5 4 3 1 2
