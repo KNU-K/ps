db5eaa3c55b6e38c864a80012f235b5ddb14f734
강태현 <126179088+KNU-K@users.noreply.github.com>
2024-01-24 00:43:06 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+int arr[1001] = { 0, };
+int lis[1001] = { 0, };
+int rec[1001] = { 0, };
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	lis[0] = arr[0];
+	int k = 0;
+	int j = 1;
+	rec[0] = 0;
+	for (int i = 1; i < n; i++) {
+		if (lis[k]>arr[i]) {
+			lis[++k] = arr[i];
+			rec[i] = k;
+		}
+		else {
+			int* idx = lower_bound(lis, lis + k, arr[i],  greater<int>());
+			*(idx) = arr[i];
+			
+			rec[i] = idx-1 - lis;
+		}
+	}
+	cout << k + 1<<"\n";
+	//stack<int> ans;
+	//for (int i = n-1; i >=0; i--) {
+	//	if (rec[i] == k) {
+	//		ans.push(arr[i]);
+	//		k--;
+	//	}
+	//}
+	//while (!ans.empty()) {
+	//	cout << ans.top() << " ";
+	//	ans.pop();
+	//}
+
+}
