d969eed99cea2277fc9c53850ca350668ff104b4
강태현 <126179088+KNU-K@users.noreply.github.com>
2024-01-30 23:23:02 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	int i = 1;
+	while (n--) {
+		int x, y;
+		cin >> x >> y;
+		cout <<"Case #"<<i<<": "<<x<<" + "<<y<<" = "<< x + y << "\n";
+		i++;
+	}
+
+}
