ac0538dfcb9e5153e73a578b3f2992108beb3f59
강태현 <126179088+KNU-K@users.noreply.github.com>
2024-02-01 18:33:50 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,31 @@
+
+
+
+#include <iostream>
+using namespace std;
+int arr[101][101];
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			int x;
+			cin >> x;
+			arr[i][j] += x;
+		}
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+}
