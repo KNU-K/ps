3265f8ecc0dfcba2e40fccdef29644cd2ab61e2e
taehyun <126179088+KNU-K@users.noreply.github.com>
2024-07-30 22:50:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,11 @@
+function solution(absolutes, signs) {
+    var answer = 0;
+    signs.forEach((sign,index)=>{
+        if(sign){
+            answer += absolutes[index];
+        }else{
+            answer -= absolutes[index];
+        }
+    })
+    return answer;
+}
